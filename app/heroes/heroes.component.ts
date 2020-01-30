47d5be8ca0c7b1c54f240cc3299e6459c4497818
marcin-napolski <marcin.napolski@gmail.com>
2020-01-30 09:31:20 +0100

    adding service

--- app/heroes/heroes.component.ts
+++ app/heroes/heroes.component.ts
@@ -3,6 +3,7 @@ import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { Race } from '../race';
 import { RACES } from '../races';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -22,13 +23,18 @@ export class HeroesComponent implements OnInit {
   //   race: 'elf'
   // };
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes); //this must happen asynchronisly
+  }
 }
